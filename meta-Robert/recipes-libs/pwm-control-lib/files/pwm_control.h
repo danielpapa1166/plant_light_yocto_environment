abc26e9bf4d9e5710f0fbb0f883f71024ff9556d
Dániel Pápa <danielpapa1166@gmail.com>
2026-02-27 13:10:59 +0100

    pwm control lib finished, working

--- meta-Robert/recipes-libs/pwm-control-lib/files/pwm_control.h
+++ meta-Robert/recipes-libs/pwm-control-lib/files/pwm_control.h
@@ -6,5 +6,8 @@ void hello_world();
 int pwm_control_init_device();
 int pwm_control_set_period(int period_ns);
 int pwm_control_set_duty_cycle(int duty_cycle_ns);
-
+int pwm_control_set_duty_percent(int duty_percent);
+int pwm_control_get_period();
+int pwm_control_deinit_device();
+int pwm_control_isenabled(); 
 #endif // PWM_CONTROL_H
